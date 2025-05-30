a9c913b6a08f74b4ab1a95cb9de3f7d4811139ef
Patrick Floirendo <timmyjb08@gmail.com>
2025-05-30 15:12:31 -0700

    Update and rename solvedproblem1.py to trainingxo.py

---
+++ trainingxo.py
@@ -0,0 +1,24 @@
+#Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+
+"""Examples input/output:
+
+XO("ooxx") => true
+XO("xooxx") => false
+XO("ooxXm") => true
+XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+XO("zzoo") => false"""
+
+def xo(s):
+    characters = list(s)
+    num_x = 0
+    num_o = 0
+    for character in characters:
+        if character.lower() == "o".lower():
+            num_o += 1
+        elif character.lower() == "x".lower():
+            num_x += 1
+    if num_x == num_o:
+        return True
+    else:
+        return False
+xo(s)
